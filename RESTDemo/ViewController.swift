17d918b61573428f0ce8076316a2d067e285c041
John Cottrell <jcottrell@yahoo.com>
2017-09-19 12:16:49 -0500

    Initial Commit

---
+++ RESTDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RESTDemo
+//
+//  Created by John Cottrell on 9/19/17.
+//  Copyright © 2017 John Cottrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
